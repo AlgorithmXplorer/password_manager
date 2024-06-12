8e2ff48777de01e30ef747d6431253f5c82dce02
Mustafa <bulutmustafa.0134@gmail.com>
2024-06-12 16:22:00 +0300

    input of question's answer has been done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 file_path = "C:/Users/VICTUS/Masaüstü/PYTHON_ALL/eğitim dışı kodlar/password_manager/passwords.txt"
-
+islemler = ["şifrelere bakma", "şifre ekleme", "şifre düzenleme"] 
 
 def file_read(path):
     with open(path,"r+",encoding = "utf-8") as file:
@@ -10,5 +10,11 @@ def file_read(path):
             print(f"internet sitesi: {i[0]}\nkullanıcı adı: {i[1]}\nşifre:{i[2]}\ndeğiştirme tarihi: {i[3]}")
             print("-"*50)
             
+def question():
+    print("işlemler:")
+    for index,islem in enumerate(islemler):
+        print(f"{index + 1}- {islem}")
+    islem_inp = int(input("hangi işlem(sayı giriniz): ")) - 1
+    return islem_inp
 
 
