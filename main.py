48edb74b9d44a78055f40b82561a9087d5491f73
Mustafa <bulutmustafa.0134@gmail.com>
2024-06-12 16:41:56 +0300

    main function has been created and "if" blocks has been writed. try "except blocks" will be written

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 file_path = "C:/Users/VICTUS/Masaüstü/PYTHON_ALL/eğitim dışı kodlar/password_manager/passwords.txt"
-islemler = ["şifrelere bakma", "şifre ekleme", "şifre düzenleme"] 
-
+islemler = ["şifrelere bakma", "şifre ekleme", "şifre düzenleme","çıkış"] 
+#* fonksiyonların sonuna print ile "\n\n" str verisini yazdır 
 def file_read(path):
     with open(path,"r+",encoding = "utf-8") as file:
 
@@ -9,12 +9,30 @@ def file_read(path):
             i = i.split(",")
             print(f"internet sitesi: {i[0]}\nkullanıcı adı: {i[1]}\nşifre:{i[2]}\ndeğiştirme tarihi: {i[3]}")
             print("-"*50)
-            
+    print("\n\n")      
 def question():
     print("işlemler:")
     for index,islem in enumerate(islemler):
         print(f"{index + 1}- {islem}")
-    islem_inp = int(input("hangi işlem(sayı giriniz): ")) - 1
+    islem_inp = int(input("hangi işlem(sayı giriniz): "))
+    print("\n\n")
     return islem_inp
 
-
+def main(path):
+    num = question()
+    if  num == 1:
+        file_read(path)
+        main(path)
+    
+    elif num == 2:
+        print("şifre ekleme fonksiyonu")
+        main(path)
+    
+    elif num == 3:
+        print("şifre düzenleme fonksiyonu")
+        main(path)
+    elif num == 4:
+        print("çıkıldı".upper())
+    
+    
+main(file_path)
