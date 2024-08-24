909bbd1d3f77f618cdb1a7b1d3c1d1d876bc1c5f
Mustafa <bulutmustafa.0134@gmail.com>
2024-08-25 01:21:35 +0300

    working logic of the menu is changed

--- menu.py
+++ menu.py
@@ -1,33 +1,37 @@
 import main
 
-operations = ["register a password", "read passwords" , "read deleted passwords", "delete a password", "change a password", "bring back a password","exit"]
 repo = main.password_repo(path=main.file_path , delete_path= main.delete_data_path)
-
+def registering():
+    inp = inputs()
+    repo.register_a_password(param= main.password(url=inp[0], name= inp[1], mail= inp[2], password= inp[-1]))
 def inputs():
     name = input("kullanıcı ismi: ")
     url = input("site url'si: ")
     mail = input("mail: ")
     password = input("şifre: ")
     return [url,name,mail,password]
+
+operations = {"register a password" : registering,
+              "read passwords": repo.read_passwords,
+              "read deleted passwords": repo.read_deleted_passwords,
+              "delete a password": repo.delete_a_password,
+              "change a password": repo.change_a_password,
+              "bring back a password": repo.bring_back_a_password,
+              "exit": "q"}
 while True:
-    for index,opr in enumerate(operations,1):
-        print(f"{index}- {opr}")
-    choice = input("hangi işlem: ")
-    if choice == "1":
-        inp = inputs()
-        repo.register_a_password(param= main.password(url=inp[0], name= inp[1], mail= inp[2], password= inp[-1]))
-    elif choice == "2":
-        repo.read_passwords()
-    elif choice == "3":
-        repo.read_deleted_passwords()
-    elif choice == "4":
-        repo.delete_a_password()
-    elif choice == "5":
-        repo.change_a_password()
-    elif choice == "6":
-        repo.bring_back_a_password()
-    elif choice == "7":
+    keys = []
+    print(keys)
+    x = 1
+    for key,value in operations.items():
+        print(f"{x}-{key}")
+        keys.append(key)
+        x += 1
+    choice = int(input("hangi işlem(sayı): ")) -1
+    func = operations[keys[choice]]
+    if func == "q":
         break
+    func()
+
 
 
 
